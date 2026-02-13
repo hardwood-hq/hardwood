2d0fd54980aac857ee797a8a574e2bf6e8ff8306
Rion Williams <rionmonster@gmail.com>
2026-02-13 12:41:53 -0600

    hardwood#16 Added previously missing licensing header

--- core/src/test/java/dev/morling/hardwood/internal/reader/PageCursorTest.java
+++ core/src/test/java/dev/morling/hardwood/internal/reader/PageCursorTest.java
@@ -1,3 +1,10 @@
+/*
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Copyright The original authors
+ *
+ *  Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package dev.morling.hardwood.internal.reader;
 
 import java.lang.reflect.Field;
